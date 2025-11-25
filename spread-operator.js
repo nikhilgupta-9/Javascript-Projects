5b00db0331f315340eb648e310856fcc1062f0a1
Nikhil Gupta <iamnikhilgupta9@gmail.com>
2025-11-25 06:07:29 +0530

    latest changes

--- spread-operator.js
+++ spread-operator.js
@@ -1,1 +1,11 @@
 console.log("spread operater in js file");
+
+// exmaple 1 for spreay operator 
+let arr = [10,20,30,40];
+
+function avg(a,b,c){
+    return(a+b+c)/3;
+} 
+
+let a = avg(...arr);
+console.log(a);
